2febc456056e245915656fe7a0ec1b93b938b046
Julie Anne Co <cojulieanne@gmail.com>
2021-09-10 02:51:19 +0800

    Update consistency_chapter_1.py

--- Qualification Round/consistency_chapter_1.py
+++ Qualification Round/consistency_chapter_1.py
@@ -6,7 +6,7 @@ FB Hackecup 2021: A1 Consistency Chapter 1
 Facebook Hacker Cup 2021 - Qualification Round
 https://www.facebook.com/codingcompetitions/hacker-cup/2021/qualification-round/problems/A1
 ------------------------------------------------------
-"""
+""" 
 
 import sys
 
